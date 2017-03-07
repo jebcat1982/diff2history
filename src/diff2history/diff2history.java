984a2eac15485ec704b4a0a9ad1457cda78495ab
gloryer <shuai.li@uwaterloo.ca>
2017-03-07 18:28:11 -0500

    2nd

--- src/diff2history/diff2history.java
+++ src/diff2history/diff2history.java
@@ -1,13 +1,37 @@
 package diff2history;
 
+import java.io.File;
+import java.io.IOException;
+
 public class diff2history {
 
-	public diff2history() {
-		// TODO Auto-generated constructor stub
+	public String repoID;
+	public String lib;
+	public File dir;
+	public File[] commits;
+	public File out;
+	public diff2history(String repoID, String lib) throws IOException {
+		this.repoID = repoID;
+		this.lib = lib;
+		dir = new File(this.repoID);
+		commits = dir.listFiles();
+		out = new File("/home/shuaili/X/" + lib + "/" + repoID);
+		out.createNewFile();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String repoID = args[0];
+		String lib = args[1];
+		diff2history d2h = null;
+		try {
+			d2h = new diff2history(repoID, lib);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (d2h.commits != null) {
+			
+		}
 
 	}
 
